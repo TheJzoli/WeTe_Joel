75f2c631956cd11124ec9dbe3a48b344e9237ba0
Joel <33924866+TheJzoli@users.noreply.github.com>
2019-03-29 11:02:16 +0200

    tehtavat 7 tehty

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -38,9 +38,32 @@ function addTask() {
     
 } // End of addTask() function.
 
+ // Function removes duplicate entries from global array
+function removeDuplicates() {
+    var unique = {};
+    tasks.forEach(function (i) {
+        if(!unique[i]) {
+                unique[i] = true;
+        }
+    });
+    tasks = Object.keys(unique);
+
+    // Update page:
+    var output = document.getElementById('output');
+    var message;
+
+    message = '<h2>To-Do</h2><ol>';
+    for (var i = 0, count = tasks.length; i < count; i++) {
+        message += '<li>' + tasks[i] + '</li>';
+    }
+    message += '</ol>';
+    output.innerHTML = message;
+}
+
 // Initial setup:
 function init() {
     'use strict';
     document.getElementById('theForm').onsubmit = addTask;
+    document.getElementById('duplicates').onclick = removeDuplicates;
 } // End of init() function.
 window.onload = init;
