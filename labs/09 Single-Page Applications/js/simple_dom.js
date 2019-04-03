4e5ee89b7452fe5c577df62827ff8023e08f36d4
Joel <33924866+TheJzoli@users.noreply.github.com>
2019-04-03 15:39:04 +0300

    kasi tehty, ja eka ysist

--- labs/09 Single-Page Applications/js/simple_dom.js
+++ labs/09 Single-Page Applications/js/simple_dom.js
@@ -2,23 +2,20 @@
 
 console.log(document);
 
-document.getElementById('save').onclick = save;
-document.querySelector('#userForm input[type="email"]').onkeypress = function() {
+document.querySelector('#userForm input[type="email"]').onkeyup = function() {
 	console.log('updating email');
 	var email = document.querySelector('#userForm input[type="email"]').value;
-	document.querySelector('#summary p').innerHTML = email;
+	document.querySelectorAll('#summary p')[0].innerHTML = email;
 };
 
+document.querySelector('#userForm input[type="text"]').onkeyup = function() {
+	console.log('updating name');
+	var name = document.querySelector('#userForm input[type="text"]').value;
+	document.querySelector('#summary h1').innerHTML = name;
+};
 
-function save() {
-	console.log('save');
-	// get a DOM object representing a form field.
-	var name = document.querySelector('#userForm input[type="text"]');
-	console.log(name);
-	document.querySelector('#summary h1').innerHTML = name.value;
-	var data = document.querySelectorAll('#userForm input');
-	console.log(data);
-	var paragraphs = document.querySelectorAll('#summary p');
-	console.log('found '+paragraphs.length+' p tags');
-	paragraphs[1].innerHTML = 'Hello World!';
-}
+document.querySelector('#userForm input[type="password"]').onkeyup = function() {
+	console.log('updating password');
+	var password = document.querySelector('#userForm input[type="password"]').value;
+	document.querySelectorAll('#summary p')[1].innerHTML = password;
+};
