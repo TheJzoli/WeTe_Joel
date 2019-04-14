477cb6416a1c82adf9fe33886b96d84605833955
Joel <33924866+TheJzoli@users.noreply.github.com>
2019-04-14 19:34:44 +0300

    yksitoista tehty

--- labs/11 Unit Testing/jasmine-standalone-3.4.0/spec/notes.spec.js
+++ labs/11 Unit Testing/jasmine-standalone-3.4.0/spec/notes.spec.js
@@ -7,11 +7,67 @@ describe('notes module', function () {
         notes.add('fourth note');
         notes.add('fifth note');
     });
-    it("should be able to add a new note", function () {
-        expect(notes.add('sixth note')).toBe(true);
-        expect(notes.count()).toBe(6);
+
+    describe('adding a note', function() {
+        it("should be able to add a new note", function () {
+            expect(notes.add('sixth note')).toBe(true);
+            expect(notes.count()).toBe(6);
+        });
+        it("should ignore blank notes", function () {
+            expect(notes.add('')).toBe(false);
+            expect(notes.count()).toBe(5);
+        });
+        it('should ignore notes containing only whitespace', function () {
+            expect(notes.add('   ')).toBe(false);
+            expect(notes.count()).toBe(5);
+        });
+        it('should require a string parameter', function () {
+            expect(notes.add()).toBe(false);
+            expect(notes.count()).toBe(5);
+        });
     });
-    it('should ignore blank notes');
-    it('should ignore notes containing only whitespace');
-    it('should require a string parameter');
+
+    describe('deleting a note', function() {
+        it('should be able to delete the first index', function () {
+            expect(notes.remove(0)).toBe(true);
+            expect(notes.count()).toBe(4);
+        });
+        it('should be able to delete the last index', function () {
+            expect(notes.remove(notes.count()-1)).toBe(true);
+            expect(notes.count()).toBe(4);
+        });
+        it('should return false if index is out of range', function () {
+            expect(notes.remove(6)).toBe(false);
+            expect(notes.count()).toBe(5);
+        });
+        it('should return false if the parameter is missing', function () {
+            expect(notes.remove()).toBe(false);
+            expect(notes.count()).toBe(5);
+        });
+    });
+
+    describe('finding a note', function() {
+        it('should be able to find notes', function () {
+            expect(notes.find('note')).toBe(true);
+            expect(notes.find('th')).toBe(true);
+            expect(notes.find('four')).toBe(true);
+            expect(notes.count()).toBe(5);
+        });
+        it('should ignore case', function () {
+            expect(notes.find('Note')).toBe(true);
+            expect(notes.count()).toBe(5);
+        });
+        it('should return false if no match is found', function () {
+            expect(notes.find('six')).toBe(false);
+            expect(notes.count()).toBe(5);
+        });
+        it('should return false if searching for blank notes', function () {
+            expect(notes.find('')).toBe(false);
+            expect(notes.count()).toBe(5);
+        });
+        it('should return false if the parameter is missing', function () {
+            expect(notes.find()).toBe(false);
+            expect(notes.count()).toBe(5);
+        });
+    });
 });
--- labs/11 Unit Testing/jasmine-standalone-3.4.0/src/notes.js
+++ labs/11 Unit Testing/jasmine-standalone-3.4.0/src/notes.js
@@ -3,17 +3,47 @@ var notes = (function() {
 
     return {
         add: function(note) {
-            var item = {timestamp: Date.now(), text: note};
-            list.push(item);
-            return true;
+            if (note !== undefined && note && note.trim()) {
+                var item = {timestamp: Date.now(), text: note};
+                list.push(item);
+                return true;
+            }
+            return false;
         },
-        remove: function(index) {},
+
+        remove: function(index) {
+            if (index >= 0 && index < list.length) {
+                list.splice(index, 1);
+                return true;
+            }
+            return false;
+        },
+
         count: function() {
             return list.length;
         },
-        list: function() {},
-        find: function(str) {},
-        clear: function() {}
-    }
+
+        list: function() {
+            return list;
+        },
+
+        find: function(str) {
+            if (str !== undefined && str) {
+                var notesContainingStr = [];
+                for (var i = 0; i < list.length; i++) {
+                    if (list[i].text.toLowerCase().indexOf(str.toLowerCase()) !== -1) {
+                        notesContainingStr.push(list[i]);
+                    }
+                }
+                if (notesContainingStr.length > 0) {
+                    return true;
+                }
+            }
+            return false;
+        },
+
+        clear: function() {
+            list.splice(0, list.length);
+        }
     }
 }());
