a31c85987b6111b4dd8e0018c2f0f3c5010ab037
Joel <33924866+TheJzoli@users.noreply.github.com>
2019-04-17 16:17:14 +0300

    14 tehty

--- labs/14 Modular PHP/diceclasses.inc.php
+++ labs/14 Modular PHP/diceclasses.inc.php
@@ -1,18 +1,30 @@
 <?php
 
 class Dice {
-    private  $faces;
+    private  $faces, $bias = null;
     private  $freqs = array();
     
     // Constructor
-    public function __construct($faces) {
+    public function __construct($faces, $bias) {
         $this->faces = $faces;
+        if($bias !== null) {
+            $this->bias = $bias;
+        }
     }
     
     public function cast() {
-        $res = rand(1,$this->faces);
-        $this->freqs[$res]++;
-        return $res;
+        if ($this->bias !== null) {
+            $random = rand(1, 100);
+            if ($random <= $this->bias*100) {
+                return $this->faces;
+            } else {
+                return rand(1, $this->faces-1);
+            }
+        } else {
+            $res = rand(1,$this->faces);
+            $this->freqs[$res]++;
+            return $res;
+        }
     }
     
     public function getFreq($eyes) {
@@ -20,6 +32,30 @@ class Dice {
         if ($freq=="")
             $freq = 0;
         return $freq;
+    }
+
+    public function average() {
+        $total = 0;
+        $amount = 0;
+        for ($i=1; $i<=$this->faces; $i++) {
+            if ($this->freq[$i]=="") {
+                $total += $i * 0;
+                $amount += 0;
+            } else {
+                $total += $i * $this->freqs[$i];
+                $amount += $this->freqs[i];
+            }
+        }
+        return $total / $amount;
+    }
+}
+
+class PhysicalDice extends Dice {
+    private $material;
+
+    function __construct($faces, $material, $bias) {
+        parent::__construct($faces, $bias);
+        $this->material = $material;
     }
 }
 
--- labs/14 Modular PHP/diceplay.php
+++ labs/14 Modular PHP/diceplay.php
@@ -4,11 +4,25 @@ include("diceclasses.inc.php");
 
 $faces = $_GET["faces"];
 $throws = $_GET["throws"];
+$p = $_GET["bias"];
+$mat = $_GET["material"];
 
 $results = array();
 
 // make dice
-$dice = new Dice($faces);
+if (isset($p)){
+    if (isset($mat)) {
+        $pDice = new PhysicalDice($faces, $mat, $p);
+    } else {
+        $dice = new Dice($faces, $p);
+    }
+} else {
+    if (isset($mat)) {
+        $pDice = new PhysicalDice($faces, $mat, null);
+    } else {
+        $dice = new Dice($faces, null);
+    }
+}
 for ($i = 1; $i<=$throws; $i++) {
     $res = $dice->cast();
     $results[] = array('id' => strval($i), 'res' => strval($res));
@@ -17,7 +31,7 @@ $freqs = array();
 for ($i = 1; $i<=$faces; $i++) {
     $freqs[] = array ('eyes' => strval($i), 'frequency' => strval($dice->getFreq($i)));
 }
-echo json_encode(array('faces'=>$faces,'results'=>$results,'frequencies'=>$freqs));
+echo json_encode(array('faces'=>$faces,'results'=>$results,'frequencies'=>$freqs, 'average number of eyes'=>$dice->average()));
 
 
 
